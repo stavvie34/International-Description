fd2598ef32e19b009650d1750d86ed4495f82a3b
stavvie34 <alexanderlee58col@gmail.com>
2017-02-07 16:04:26 -0800

    commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/andytran/pen/LJFeg.
+
+ A simple flat pricing table with jQuery that enables the content to slide down onClick.
---
+++ js/index.js
@@ -0,0 +1,24 @@
+$( ".pre-screen-title-1" ).click(function() {
+  $(".pre-screen-content-1").slideToggle();
+});
+$( ".pre-screen-title-2" ).click(function() {
+  $(".pre-screen-content-2").slideToggle();
+});
+$( ".koc-title-1" ).click(function() {
+  $(".koc-content-1").slideToggle();
+});
+$( ".koc-title-2" ).click(function() {
+  $(".koc-content-2").slideToggle();
+});
+$( ".ambassadorship-title-1" ).click(function() {
+  $(".ambassadorship-content-1").slideToggle();
+});
+$( ".ambassadorship-title-2" ).click(function() {
+  $(".ambassadorship-content-2").slideToggle();
+});
+$( ".skill-talent-title-1" ).click(function() {
+  $(".skill-talent-content-1").slideToggle();
+});
+$( ".skill-talent-title-2" ).click(function() {
+  $(".skill-talent-content-2").slideToggle();
+});
